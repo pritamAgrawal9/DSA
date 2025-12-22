0222c0bf10ee0e654a1fb4d3f6c1eaaf8b0a7e59
pritam <pritamtg99999@gmail.com>
2025-12-22 15:21:39 +0530

    Merge Sort completed

--- Searching and Sorting.py
+++ Searching and Sorting.py
@@ -28,14 +28,46 @@ n = len(nums)
 # print(nums)
 
 # Selection Sort
-for i in range(n):
-    min_num = nums[i]
-    ind = i
-    for j in range(i+1,n):
-        if nums[j] < min_num:
-            min_num = nums[j]
-            ind = j
+# for i in range(n):
+#     min_num = nums[i]
+#     ind = i
+#     for j in range(i+1,n):
+#         if nums[j] < min_num:
+#             min_num = nums[j]
+#             ind = j
+#
+#     temp = nums[i]
+#     nums[i] = nums[ind]
+#     nums[ind]=temp
 
-    temp = nums[i]
-    nums[i] = nums[ind]
-    nums[ind]=temp
+# Merge Sort
+def merge(nums,l,mid,r):
+        a=nums[l:mid+1]
+        b=nums[mid+1:r+1]
+        i,j,k=0,0,l
+        while k <= r:
+            if j==len(b):
+                nums[k] = a[i]
+                i += 1
+                k += 1
+            elif i==len(a):
+                nums[k] = b[j]
+                j += 1
+                k += 1
+            elif a[i] < b[j]:
+                nums[k] = a[i]
+                i+=1
+                k+=1
+            else:
+                nums[k]=b[j]
+                j+=1
+                k+=1
+def merge_sort(nums,l,r):
+    if l>=r:
+        return
+    mid = (l+r)//2
+    merge_sort(nums,l,mid)
+    merge_sort(nums,mid+1,r)
+    merge(nums,l,mid,r)
+merge_sort(nums,0,len(nums)-1)
+print(nums)
